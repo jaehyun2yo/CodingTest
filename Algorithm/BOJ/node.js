7748def188fed44fd9db3b7685f364fddeaaa165
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2022-04-02 17:24:15 +0900

    BOJ 2022-04-02

--- Algorithm/BOJ/node.js
+++ Algorithm/BOJ/node.js
@@ -1,6 +1,25 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+// We love krill
+console.log("강한친구 대한육군");
+console.log("강한친구 대한육군");
 
-for (let i = 0; i < input.length; i++) {
-  console.log("강한친구 대한육군");
-}
+//고양이
+console.log(`\\    /\\`);
+console.log(` )  ( ')`);
+console.log(`(  /  )`);
+console.log(` \\(__)|`);
+
+//개
+console.log(`|\\_/|`);
+console.log(`|q p|   /}`);
+console.log(`( 0 )"""\\`);
+console.log('|"^"`    |');
+console.log(`||_/=\\\\__|`);
+
+// A+B 
+
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ");
+console.log(input[0] + input[1]);
