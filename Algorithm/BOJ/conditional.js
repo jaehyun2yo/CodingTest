3d1f7f22c721e70bafd05f99fd5e4a08caad3b4b
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2022-04-05 20:23:07 +0900

    BOJ 2022-04-05

--- Algorithm/BOJ/conditional.js
+++ Algorithm/BOJ/conditional.js
@@ -52,4 +52,30 @@ if ((year % 4 == 0 && year % 100 !== 0) || year % 400 == 0) {
   console.log("0");
 }
 
-//
+//사분면 고르기
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(parseInt(line));
+}).on("close", function () {
+  const x = input[0];
+  const y = input[1];
+
+  if (x > 0 && y > 0) {
+    console.log(1);
+  } else if (x < 0 && y > 0) {
+    console.log(2);
+  } else if (x < 0 && y < 0) {
+    console.log(3);
+  } else if (x > 0 && y < 0) {
+    console.log(4);
+  }
+
+  process.exit();
+});
