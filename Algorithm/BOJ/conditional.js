ee8718505b3362b2f062e295fc6cfde9ce1b7e0f
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2022-04-06 18:28:08 +0900

    BOJ 2022-04-06

--- Algorithm/BOJ/conditional.js
+++ Algorithm/BOJ/conditional.js
@@ -79,3 +79,23 @@ rl.on("line", function (line) {
 
   process.exit();
 });
+
+// 알람 시계
+
+let input = require("fs").readFileSync("dev/stdin").toString().split(" ");
+
+let hour = Number(input[0]); // Hour
+let minute = Number(input[1]); // Minute
+
+minute -= 45;
+
+if (minute < 0) {
+  minute += 60;
+  hour--;
+
+  if (hour === -1) {
+    hour = 23;
+  }
+}
+
+console.log(hour + " " + minute);
