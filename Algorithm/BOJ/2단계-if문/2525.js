2512b38de3743f7e307da9034a8f15d4cfb40527
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2022-04-26 23:31:02 +0900

    update: BOJ

--- Algorithm/BOJ/2단계-if문/2525.js
+++ Algorithm/BOJ/2단계-if문/2525.js
@@ -23,3 +23,47 @@ if (m + cookTime >= 60) {
   m += cookTime;
 }
 console.log(`${h} ${m}`);
+
+// readline
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let cnt = 0;
+let H = 0;
+let M = 0;
+let timer = 0;
+rl.on("line", function (line) {
+  cnt++;
+  if (cnt === 2) {
+    timer = parseInt(line);
+    rl.close();
+  }
+  line = line.split(" ");
+  H = parseInt(line[0]);
+  M = parseInt(line[1]);
+}).on("close", function () {
+  console.log(solution(H, M, timer));
+  process.exit();
+});
+
+function solution(h, m, time) {
+  let answer = "";
+  let Hadd = Math.floor((m + time) / 60);
+  let Madd = (m + time) % 60;
+
+  if (m + time >= 60) {
+    h = h + Hadd;
+    m = Madd;
+    if (h > 23) {
+      h %= 24;
+    }
+  } else if (m + time < 60) {
+    m = m + time;
+  }
+  answer = h + " " + m;
+  return answer;
+}
